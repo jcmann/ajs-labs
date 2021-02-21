ba11ce2ec5225a28c3cbe552829876faf49bd791
jcmann <jen.c.mann@gmail.com>
2021-02-21 16:58:08 -0600

    Completed lab 5.

---
+++ lab-05/js/json.js
@@ -0,0 +1,40 @@
+const init = () => {
+
+    // Collection of JSON objects containing student information 
+    const students = [
+        {
+            "name" : "Rigby",
+            "id" : 1,
+            "email" : "rigby@madisoncollege.edu"
+        }, 
+        {
+            "name" : "Tank",
+            "id" : 2,
+            "email" : "tank@madisoncollege.edu"
+        }, 
+        {
+            "name" : "Mozzy",
+            "id" : 3,
+            "email" : "mozzy@madisoncollege.edu"
+        }, 
+        {
+            "name" : "Nori",
+            "id" : 4,
+            "email" : "nori@madisoncollege.edu"
+        }, 
+        {
+            "name" : "Pippin",
+            "id" : 5,
+            "email" : "pippin@madisoncollege.edu"
+        }
+    ]; 
+
+    students.forEach(current => {
+        let p = document.createElement('p'); 
+        p.innerHTML = `<p>ID: ${current.id}. Email: ${current.email}</p>`; 
+        document.querySelector('body').append(p); 
+    }); 
+}
+
+// Immediately calls init when page is loaded. 
+window.addEventListener('load', init); 
